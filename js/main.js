03df803cfe445ebae5454dddf2d2a67e8ad6a749
seanb0a <sboaden@iprimus.com.au>
2017-07-12 21:29:09 +0800

    Completed design
    -Completed general layout/design
    -Added first C++ project, "Acid Rain"

--- js/main.js
+++ js/main.js
@@ -3,37 +3,19 @@ $(window).load(function() {
   $('.menu-open, .ion-ios-arrow-right').click(function() {
     $('.menu-overlay').css("animation", "open-menu 500ms ease-out forwards");
     $('.menu-close').css("animation", "getClose 250ms ease-out 500ms forwards");
-    $('.first').css("animation", "scaleLeftNav 0.3s ease-out 0s forwards");
-    $('.second').css("animation", "scaleLeftNav 0.3s ease-out 0.25s forwards");
-    $('.third').css("animation", "scaleLeftNav 0.3s ease-out 0.5s forwards");
-    $('.fourth').css("animation", "scaleLeftNav 0.3s ease-out 0.75s forwards");
+    $('.first, .fourth').css("animation", "scaleDown 0.25s ease-out 0.5s forwards");
   });
 
   $('.menu-close, .ion-ios-arrow-left').click(function() {
-    $('.first, .second, .third, .fourth').fadeOut("fast");
+    $('.fourth, .first').fadeOut("fast");
 
     setTimeout(function() {
 
       $('.menu-overlay').css("animation", "close-menu 500ms ease-out forwards");
       $('.menu-close').css("animation", "loseClose 250ms ease-out forwards");
 
-      setTimeout(function() {
-
-        $('.first, .second, .third, .fourth').fadeIn("fast");
-
-      }, 500);
-
     }, 250);
 
   });
-
-  // var rnd = Math.random() * (8000 - 2000) + 2000;
-
-  // setTimeout(function() {
-
-  //   $('#loader').hide();
-  //   $('#page').removeClass('hidden');
-
-  // }, rnd);
 
 });
